70ccc76a5567c94c5ce7c9dff491fcc11a4c3fa5
Shidhin <shidhincr@gmail.com>
2014-08-19 17:14:01 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ *	1. Read the file from the file system
+ *	2  Parse the file : get the url and status code mapping -> map object
+ *	3. Process the map object one by one
+ *		1. Http call to the url and assert the status code from the map object
+ *	
+ */
